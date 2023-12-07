526996f27c1916135bc27a6ba5416e229f08c5cf
Ghaz Riz <ghaz.riz@naviscom.com>
2023-12-07 12:50:17 +0500

    another commit

--- unittestwriter.go
+++ unittestwriter.go
@@ -10,24 +10,6 @@ import (
 	"github.com/naviscom/dbSchemaReader"
 )
 
-type FK_Hierarchy struct{
-	TableName				string
-	RelatedTablesLevels		[]RelatedTables
-}
-
-type RelatedTables struct {
-	RelatedTableList	[]RelatedTable
-}
-
-type RelatedTable struct {
-	FK_Related_TableName					string
-	FK_Related_SingularTableName			string
-	FK_Related_Table_Column					string
-	FK_Related_TableName_Plural_Object		string
-	FK_Related_TableName_Singular_Object	string
-}
-
-
 func main_testFunc(dirPath string){
 	outputFileName := dirPath + "/db/sqlc/main_test.go"
 	outputFile, errs := os.Create(outputFileName)
@@ -56,7 +38,7 @@ func main_testFunc(dirPath string){
 	_, _ = outputFile.WriteString(")"+"\n")
 	_, _ = outputFile.WriteString("\n")
 
-	_, _ = outputFile.WriteString("func TestMain(m *testing.M) {" + "\n")
+	_, _ = outputFile.WriteString("func TestMain(m *testing.M ) {" + "\n")
 	_, _ = outputFile.WriteString("	conn, err := sql.Open(dbDriver, dbSource)" + "\n")
 	_, _ = outputFile.WriteString("	if err != nil {" + "\n")
 	_, _ = outputFile.WriteString(`		log.Fatal("cannot connect to db:", err)` + "\n")
@@ -70,61 +52,52 @@ func main_testFunc(dirPath string){
 	fmt.Println("main_test.go file has been generated successfully")
 }
 
-func printTestFuncForCreate(tableX[]dbSchemaReader.Table_Struct, i int, outputFile *os.File){
-	var relatedTable RelatedTable
-	var relatedTables RelatedTables
-	var fk_Hierarchy FK_Hierarchy
-	var fk_HierarchyX []FK_Hierarchy
-	if len(tableX[i].ForeignKeys) > 0 {
-		for j :=0; j < len(tableX[i].ForeignKeys); j++{
-			relatedTable.FK_Related_TableName = tableX[i].ForeignKeys[j].FK_Related_TableName
-			relatedTable.FK_Related_SingularTableName = tableX[i].ForeignKeys[j].FK_Related_SingularTableName
-			relatedTable.FK_Related_Table_Column = tableX[i].ForeignKeys[j].FK_Related_Table_Column
-			relatedTable.FK_Related_TableName_Singular_Object = tableX[i].ForeignKeys[j].FK_Related_TableName_Singular_Object
-			relatedTable.FK_Related_TableName_Plural_Object = tableX[i].ForeignKeys[j].FK_Related_TableName_Plural_Object
-			relatedTables.RelatedTableList = append(relatedTables.RelatedTableList, relatedTable)
-		}
-		fk_Hierarchy.TableName = tableX[i].Table_name
-		fk_Hierarchy.RelatedTablesLevels = append(fk_Hierarchy.RelatedTablesLevels, relatedTables)
-		fk_HierarchyX = append(fk_HierarchyX, fk_Hierarchy)
-	}else{
-		fk_Hierarchy.TableName = tableX[i].Table_name
-		fk_Hierarchy.RelatedTablesLevels = append(fk_Hierarchy.RelatedTablesLevels, relatedTables)
-		fk_HierarchyX = append(fk_HierarchyX, fk_Hierarchy)
+func CreateRandomFunction(tableX[] dbSchemaReader.Table_Struct, i int, outputFile *os.File){
+	funcSig := "func createRandom"+tableX[i].FunctionSignature+"(t *testing.T"
+	// _, _ = outputFile.WriteString("func createRandom"+tableX[i].FunctionSignature+"(t *testing.T")
+	for k := 0; k < len(tableX[i].ForeignKeys); k++ {
+		funcSig = funcSig + ", "+ tableX[i].ForeignKeys[k].FK_Related_SingularTableName+" "+ tableX[i].ForeignKeys[k].FK_Related_TableName_Singular_Object
+		// _, _ = outputFile.WriteString(", "+ tableX[i].ForeignKeys[k].FK_Related_SingularTableName+" "+ tableX[i].ForeignKeys[k].FK_Related_TableName_Singular_Object)					
 	}
-	var c int
-	var d int
-	var e int
-	c = 0
-	for k :=0; k < len(fk_HierarchyX); k++{
-		d = len(fk_HierarchyX[k].RelatedTablesLevels)
-		e = d - c
-		if fk_HierarchyX[k].TableName == tableX[i].Table_name {
-			for l :=len(fk_HierarchyX[k].RelatedTablesLevels)-e; l < len(fk_HierarchyX[k].RelatedTablesLevels); l++{
-				for m:=0; m < len(fk_HierarchyX[k].RelatedTablesLevels[l].RelatedTableList); m++{
-					for z :=0; z < len(tableX); z++{
-						if fk_HierarchyX[k].RelatedTablesLevels[l].RelatedTableList[m].FK_Related_TableName == tableX[z].Table_name {	
-							if len(tableX[z].ForeignKeys) > 0 {
-								relatedTables.RelatedTableList = nil
-								for y :=0; y < len(tableX[z].ForeignKeys); y++{
-									relatedTable.FK_Related_TableName = tableX[z].ForeignKeys[y].FK_Related_TableName
-									relatedTable.FK_Related_SingularTableName = tableX[z].ForeignKeys[y].FK_Related_SingularTableName
-									relatedTable.FK_Related_Table_Column = tableX[z].ForeignKeys[y].FK_Related_Table_Column
-									relatedTable.FK_Related_TableName_Singular_Object = tableX[z].ForeignKeys[y].FK_Related_TableName_Singular_Object
-									relatedTable.FK_Related_TableName_Plural_Object = tableX[z].ForeignKeys[y].FK_Related_TableName_Plural_Object
-						
-									relatedTables.RelatedTableList = append(relatedTables.RelatedTableList, relatedTable)							
-								}
-								fk_HierarchyX[k].RelatedTablesLevels = append(fk_HierarchyX[k].RelatedTablesLevels, relatedTables)
-							}
-						}
-					}
+	funcSig = funcSig + ") "+tableX[i].FunctionSignature
+	_, _ = outputFile.WriteString(funcSig+" {" + "\n")
+	_, _ = outputFile.WriteString("	arg := Create"+tableX[i].FunctionSignature+"Params{" + "\n")
+	for j := 1; j < len(tableX[i].Table_Columns); j++ {
+		if 	tableX[i].Table_Columns[j].ForeignFlag {
+			for k := 0; k < len(tableX[i].ForeignKeys); k++ {
+				if 	tableX[i].ForeignKeys[k].FK_Column == tableX[i].Table_Columns[j].Column_name {
+					_, _ = outputFile.WriteString("		"+tableX[i].Table_Columns[j].ColumnNameParams+":    	"+ tableX[i].ForeignKeys[k].FK_Related_SingularTableName+"."+strings.ToUpper((tableX[i].ForeignKeys[k].FK_Related_Table_Column)+","+ "\n"))
 				}
-
 			}
-			c = d
-		}
+		} else {
+			if tableX[i].Table_Columns[j].ColumnType == "varchar" {
+				_, _ = outputFile.WriteString("		"+tableX[i].Table_Columns[j].ColumnNameParams+":    util.RandomName(8)," + "\n")
+			}
+			if tableX[i].Table_Columns[j].ColumnType == "bigint" {
+				_, _ = outputFile.WriteString("		"+tableX[i].Table_Columns[j].ColumnNameParams+":    util.RandomInteger(1, 100)," + "\n")
+			}
+			if tableX[i].Table_Columns[j].ColumnType == "real" {
+				_, _ = outputFile.WriteString("		"+tableX[i].Table_Columns[j].ColumnNameParams+":    util.RandomReal(1, 100)," + "\n")
+			}
+			if tableX[i].Table_Columns[j].ColumnType == "timestamptz" {
+				_, _ = outputFile.WriteString("		"+tableX[i].Table_Columns[j].ColumnNameParams+":    time.Now().UTC(),"+ "\n")
+			}	
+		}		
 	}
+	_, _ = outputFile.WriteString("	}" + "\n")
+	_, _ = outputFile.WriteString("	"+tableX[i].OutputFileName+", err := testQueries.Create"+tableX[i].FunctionSignature+"(context.Background(), arg)" + "\n")
+	_, _ = outputFile.WriteString("	require.NoError(t, err)" + "\n")
+	_, _ = outputFile.WriteString("	require.NotEmpty(t, "+tableX[i].OutputFileName+")" + "\n")
+	for j := 1; j < len(tableX[i].Table_Columns); j++ {
+		_, _ = outputFile.WriteString("	require.Equal(t, arg."+tableX[i].Table_Columns[j].ColumnNameParams+", "+ tableX[i].OutputFileName+"."+tableX[i].Table_Columns[j].ColumnNameParams+")"+ "\n")				
+	}
+	_, _ = outputFile.WriteString("	return "+tableX[i].OutputFileName + "\n")
+	_, _ = outputFile.WriteString("}" + "\n")
+	fmt.Println("	",funcSig+ " has been generated successfully")
+	_, _ = outputFile.WriteString("\n")
+}
+
+func printTestFuncForCreate(tableX[] dbSchemaReader.Table_Struct, i int, fk_HierarchyX[] dbSchemaReader.FK_Hierarchy, outputFile *os.File){
 	_, _ = outputFile.WriteString("func TestCreate"+tableX[i].FunctionSignature+"(t *testing.T) {"+"\n")
 	for k := 0; k < len(fk_HierarchyX); k++{
 		if fk_HierarchyX[k].TableName == tableX[i].Table_name {
@@ -159,7 +132,7 @@ func printTestFuncForCreate(tableX[]dbSchemaReader.Table_Struct, i int, outputFi
 	fmt.Println("	","func TestCreate"+tableX[i].FunctionSignature+"(t *testing.T) has been generated successfully")
 }
 
-func printTestFuncForReadGet(table []dbSchemaReader.Table_Struct, i int, outputFile *os.File){
+func printTestFuncForReadGet(table []dbSchemaReader.Table_Struct, i int, fk_HierarchyX[] dbSchemaReader.FK_Hierarchy, outputFile *os.File){
 	for j := 0; j < len(table[i].Table_Columns); j++ {
 		if table[i].Table_Columns[j].PrimaryFlag || table[i].Table_Columns[j].UniqueFlag {
 			_, _ = outputFile.WriteString("func TestGet"+table[i].FunctionSignature+strconv.Itoa(j)+"(t *testing.T) {"+"\n")
@@ -170,21 +143,21 @@ func printTestFuncForReadGet(table []dbSchemaReader.Table_Struct, i int, outputF
 	}	
 }
 
-func printTestFuncForReadList(table []dbSchemaReader.Table_Struct, i int, outputFile *os.File){
+func printTestFuncForReadList(table []dbSchemaReader.Table_Struct, i int, fk_HierarchyX[] dbSchemaReader.FK_Hierarchy, outputFile *os.File){
 	_, _ = outputFile.WriteString("func TestList"+table[i].FunctionSignature2+"(t *testing.T) {"+"\n")
 	_, _ = outputFile.WriteString("\n")
 	_, _ = outputFile.WriteString("}"+"\n")
 	_, _ = outputFile.WriteString("\n")
 }
 
-func printTestFuncForUpdate(table []dbSchemaReader.Table_Struct, i int, outputFile *os.File){
+func printTestFuncForUpdate(table []dbSchemaReader.Table_Struct, i int, fk_HierarchyX[] dbSchemaReader.FK_Hierarchy, outputFile *os.File){
 	_, _ = outputFile.WriteString("func TestUpdate"+table[i].FunctionSignature+"(t *testing.T) {"+"\n")
 	_, _ = outputFile.WriteString("\n")
 	_, _ = outputFile.WriteString("}"+"\n")
 	_, _ = outputFile.WriteString("\n")
 }
 
-func printTestFuncForDelete(table []dbSchemaReader.Table_Struct, i int, outputFile *os.File){
+func printTestFuncForDelete(table []dbSchemaReader.Table_Struct, i int, fk_HierarchyX[] dbSchemaReader.FK_Hierarchy, outputFile *os.File){
 	_, _ = outputFile.WriteString("func TestDelete"+table[i].FunctionSignature+"(t *testing.T) {"+"\n")
 	_, _ = outputFile.WriteString("\n")
 	_, _ = outputFile.WriteString("}"+"\n")
@@ -216,9 +189,9 @@ func main() {
 	//generate main_test.go file
 	/////////////////////////////
 	main_testFunc(dirPath)
-
-	
+	/////////////////////////////////////////////////	
 	//generate unit tests for go file in sqlc folder
+	/////////////////////////////////////////////////		
 	pathToSearch := dirPath+"/db/migration"
 	err := filepath.Walk(pathToSearch, func(path string, info os.FileInfo, err error) error {
 		files = append(files, path)
@@ -228,9 +201,10 @@ func main() {
 		panic(err)
 	}
 	var tableX []dbSchemaReader.Table_Struct
+	var fk_HierarchyX []dbSchemaReader.FK_Hierarchy
 	for _, element := range files {
 		if element[len(element)-6:] == `up.sql` {
-			tableX = dbSchemaReader.ReadSchema(element)				
+			tableX, fk_HierarchyX = dbSchemaReader.ReadSchema(element)				
 			for i:=0; i<len(tableX); i++{
 				outputFile, errs := os.Create(dirPath+"/db/sqlc/"+tableX[i].OutputFileName+"_test.go")
 				if errs != nil {
@@ -239,7 +213,6 @@ func main() {
 				}
 				defer outputFile.Close()
 				fmt.Println("generating ",tableX[i].OutputFileName+"_test.go")
-
 				_, _ = outputFile.WriteString("package db" + "\n")
 				_, _ = outputFile.WriteString("\n")
 				_, _ = outputFile.WriteString("import ("+"\n")
@@ -249,58 +222,13 @@ func main() {
 				_, _ = outputFile.WriteString(`	"github.com/stretchr/testify/require"` + "\n")
 				_, _ = outputFile.WriteString(`	"github.com/naviscom/`+projectFolderName+`/util"` + "\n")
 				_, _ = outputFile.WriteString(")" + "\n")
-				_, _ = outputFile.WriteString("\n")
-				//////////////////////////////////////////////
-				//generating  createRandom func for the table
-				//////////////////////////////////////////////
-				funcSig := "func createRandom"+tableX[i].FunctionSignature+"(t *testing.T"
-				// _, _ = outputFile.WriteString("func createRandom"+tableX[i].FunctionSignature+"(t *testing.T")
-				for k := 0; k < len(tableX[i].ForeignKeys); k++ {
-					funcSig = funcSig + ", "+ tableX[i].ForeignKeys[k].FK_Related_SingularTableName+" "+ tableX[i].ForeignKeys[k].FK_Related_TableName_Singular_Object
-					// _, _ = outputFile.WriteString(", "+ tableX[i].ForeignKeys[k].FK_Related_SingularTableName+" "+ tableX[i].ForeignKeys[k].FK_Related_TableName_Singular_Object)					
-				}
-				funcSig = funcSig + ") "+tableX[i].FunctionSignature
-				_, _ = outputFile.WriteString(funcSig+" {" + "\n")
-				_, _ = outputFile.WriteString("	arg := Create"+tableX[i].FunctionSignature+"Params{" + "\n")
-				for j := 1; j < len(tableX[i].Table_Columns); j++ {
-					if 	tableX[i].Table_Columns[j].ForeignFlag {
-						for k := 0; k < len(tableX[i].ForeignKeys); k++ {
-							if 	tableX[i].ForeignKeys[k].FK_Column == tableX[i].Table_Columns[j].Column_name {
-								_, _ = outputFile.WriteString("		"+tableX[i].Table_Columns[j].ColumnNameParams+":    	"+ tableX[i].ForeignKeys[k].FK_Related_SingularTableName+"."+strings.ToUpper((tableX[i].ForeignKeys[k].FK_Related_Table_Column)+","+ "\n"))
-							}
-						}
-					} else {
-						if tableX[i].Table_Columns[j].ColumnType == "varchar" {
-							_, _ = outputFile.WriteString("		"+tableX[i].Table_Columns[j].ColumnNameParams+":    util.RandomName(8)," + "\n")
-						}
-						if tableX[i].Table_Columns[j].ColumnType == "bigint" {
-							_, _ = outputFile.WriteString("		"+tableX[i].Table_Columns[j].ColumnNameParams+":    util.RandomInteger(1, 100)," + "\n")
-						}
-						if tableX[i].Table_Columns[j].ColumnType == "real" {
-							_, _ = outputFile.WriteString("		"+tableX[i].Table_Columns[j].ColumnNameParams+":    util.RandomReal(1, 100)," + "\n")
-						}
-						if tableX[i].Table_Columns[j].ColumnType == "timestamptz" {
-							_, _ = outputFile.WriteString("		"+tableX[i].Table_Columns[j].ColumnNameParams+":    time.Now().UTC(),"+ "\n")
-						}	
-					}		
-				}
-				_, _ = outputFile.WriteString("	}" + "\n")
-				_, _ = outputFile.WriteString("	"+tableX[i].OutputFileName+", err := testQueries.Create"+tableX[i].FunctionSignature+"(context.Background(), arg)" + "\n")
-				_, _ = outputFile.WriteString("	require.NoError(t, err)" + "\n")
-				_, _ = outputFile.WriteString("	require.NotEmpty(t, "+tableX[i].OutputFileName+")" + "\n")
-				for j := 1; j < len(tableX[i].Table_Columns); j++ {
-					_, _ = outputFile.WriteString("	require.Equal(t, arg."+tableX[i].Table_Columns[j].ColumnNameParams+", "+ tableX[i].OutputFileName+"."+tableX[i].Table_Columns[j].ColumnNameParams+")"+ "\n")				
-				}
-				_, _ = outputFile.WriteString("	return "+tableX[i].OutputFileName + "\n")
-				_, _ = outputFile.WriteString("}" + "\n")
-				fmt.Println("	",funcSig+ " has been generated successfully")
-
-				_, _ = outputFile.WriteString("\n")
-				printTestFuncForCreate(tableX[:], i, outputFile)
-				printTestFuncForReadGet(tableX[:], i, outputFile)
-				printTestFuncForReadList(tableX[:], i, outputFile)
-				printTestFuncForUpdate(tableX[:], i, outputFile)
-				printTestFuncForDelete(tableX[:], i, outputFile)
+				_, _ = outputFile.WriteString("\n")			
+				CreateRandomFunction(tableX[:], i, outputFile)
+				printTestFuncForCreate(tableX[:], i, fk_HierarchyX[:], outputFile)
+				printTestFuncForReadGet(tableX[:], i, fk_HierarchyX[:], outputFile)
+				printTestFuncForReadList(tableX[:], i, fk_HierarchyX[:], outputFile)
+				printTestFuncForUpdate(tableX[:], i, fk_HierarchyX[:], outputFile)
+				printTestFuncForDelete(tableX[:], i, fk_HierarchyX[:], outputFile)
 				outputFile.Close()
 			}
 		}
